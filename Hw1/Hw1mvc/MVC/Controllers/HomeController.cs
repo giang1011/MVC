02f4b74f03e21c704650c616989c38a23e687a45
giang1011 <truonggiangchammat@gmail.com>
2025-08-28 09:50:41 +0700

    Add files via upload
    HW1

---
+++ Hw1/Hw1mvc/MVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Hw1mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hw1mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello Aptech";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Hw1/Hw1mvc/MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hw1mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
