844ab2e175f77d3691204741814a161d450e2172
giang1011 <truonggiangchammat@gmail.com>
2025-09-04 11:08:54 +0700

    Add files via upload

---
+++ VietNam/Controllers/AptechController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VietNam.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VietNam.Controllers
+{
+    public class AptechController : Controller
+    {
+        [Route("aptech")]
+        public IActionResult Index()
+        {
+            ViewBag.Address = "Aptech Viet Nam - 285Doi Can, Ba Đinh, Ha Noi";
+            return View();
+        }
+
+        [Route("aptech/student")]
+        public IActionResult StudentList()
+        {
+            var students = new List<Student>
+            {
+                new Student{Id=1, Name="Hoang Duy Hieu", Age=20},
+                new Student{Id=2, Name="Nguyen Duc Minh", Age=21},
+                new Student{Id=3, Name="Luoi Qua Di", Age=22},
+            };
+            return View(students);
+        }
+
+        [Route("aptech/student/{id}")]
+        public IActionResult StudentDetail(int id)
+        {
+            var students = new List<Student>
+            {
+                new Student{Id=1, Name="Hoang Duy Hieu", Age=20},
+                new Student{Id=2, Name="Nguyen Duc Minh", Age=21},
+                new Student{Id=3, Name="Luoi Qua Di", Age=22},
+            };
+
+            var student = students.FirstOrDefault(s => s.Id == id);
+            if (student == null) return NotFound();
+
+            return View(student);
+        }
+    }
+}
---
+++ VietNam/Controllers/VietnamController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace VietNam.Controllers
+{
+    public class VietnamController : Controller
+    {
+  
+        [Route("Vietnam/thanhpho")]
+        public IActionResult ThanhPho()
+        {
+            var cities = new List<string> { "Hà Nội", "TP Hồ Chí Minh", "Đà Nẵng", "Huế", "Cần Thơ" };
+            return View(cities);
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.Info = "Việt Nam là một quốc gia ở Đông Nam Á với hơn 96 triệu dân, " +
+                           "có vị trí địa lý thuận lợi, con người thân thiện, nền văn hóa đa dạng và phong phú.";
+            return View();
+        }
+    }
+}
